597c5e7df56db96769d6d0b9a725a755ade9acfd
MateuszSajko <mateuszsajko@gmail.com>
2022-07-23 21:43:11 +0200

    Utworzenie klasy Main i klasy Car. Zadeklarowanie parametrow auta wewnątrz klasy car. Utworzenie auta audi, wyciągnięcie informacji o skrzyni biegów, zmiana wartości wszystkich atrybutów i ponowne ich odpytanie

--- src/Car.java
+++ src/Car.java
@@ -10,13 +10,31 @@ public class Car {
         this.brand = brand;
         this.automaticGearbox = automaticGearbox;
     }
-
     public void setAutomaticGearbox(boolean automaticGearbox) {
         this.automaticGearbox = automaticGearbox;
     }
     public boolean getAutomaticGearbox(){
         return automaticGearbox;
     }
+    public void setColour(String colour) {
+        this.colour= colour;
+    }
+    public String getColour(){
+        return colour;
+    }
+    public void setDoorsAmount(int doorsAmount){
+        this.doorsAmount = doorsAmount;
+    }
+    public int getDoorsAmount(){
+        return doorsAmount;
+    }
+    public void setBrand(String brand){
+        this.brand = brand;
+    }
+    public String getBrand() {
+        return brand;
+    }
+
 
 
 }
--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,16 @@ public class Main {
 
         Car audi = new Car("Red", 5,"A4", false);
         System.out.println(audi.getAutomaticGearbox());
+        System.out.println(audi.getColour());
+        System.out.println(audi.getDoorsAmount());
+        System.out.println(audi.getBrand());
         audi.setAutomaticGearbox(true);
+        audi.setColour("Blue");
+        audi.setDoorsAmount(3);
+        audi.setBrand("A3");
         System.out.println(audi.getAutomaticGearbox());
+        System.out.println(audi.getColour());
+        System.out.println(audi.getDoorsAmount());
+        System.out.println(audi.getBrand());
     }
 }
