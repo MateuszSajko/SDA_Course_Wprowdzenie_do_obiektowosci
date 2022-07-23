855b09a54a5efa9d64fb80cfc0ac24ab578a8da4
MateuszSajko <mateuszsajko@gmail.com>
2022-07-23 22:18:19 +0200

    Dodanie uzytkownikowi mozliwosci wprowadzenia zadanych parametrow

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,32 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
         Car audi = new Car("Red", 5,"A4", false);
         System.out.println(audi.getAutomaticGearbox());
         System.out.println(audi.getColour());
         System.out.println(audi.getDoorsAmount());
         System.out.println(audi.getBrand());
+
         audi.setAutomaticGearbox(true);
-        audi.setColour("Blue");
-        audi.setDoorsAmount(3);
-        audi.setBrand("A3");
-        System.out.println(audi.getAutomaticGearbox());
-        System.out.println(audi.getColour());
-        System.out.println(audi.getDoorsAmount());
-        System.out.println(audi.getBrand());
+
+        System.out.println("Wprowadz zadany kolor");
+        String kolor =scanner.nextLine();
+        audi.setColour(kolor);
+
+        System.out.println("Jaki model?");
+        String model = scanner.nextLine();
+        audi.setBrand(model);
+
+        System.out.println("Wprowadz zadana ilosc drzwi");
+        int drzwi = scanner.nextInt();
+        audi.setDoorsAmount(drzwi);
+
+        System.out.println("Twoje auto to Audi z parametrami:");
+        System.out.println("kolor: " + audi.getColour());
+        System.out.println("Ilosc drzwi: " + audi.getDoorsAmount());
+        System.out.println("Model: " + audi.getBrand());
+        System.out.println("Automatyczna skrzynia biegow:" + audi.getAutomaticGearbox());
     }
 }
