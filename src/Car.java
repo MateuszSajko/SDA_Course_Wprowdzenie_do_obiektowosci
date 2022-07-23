e036c5398f251c9d9cbcb6278e640a670a798d76
MateuszSajko <mateuszsajko@gmail.com>
2022-07-23 16:09:27 +0200

    Utworzenie klasy Main i klasy Car. Zadeklarowanie parametrow auta wewnątrz klasy car. Utworzenie auta audi, wyciągnięcie informacji o skrzyni biegów, zmiana wartości i ponowne jej odpytanie

---
+++ src/Car.java
@@ -0,0 +1,22 @@
+public class Car {
+    private String colour;
+    private int doorsAmount;
+    private String brand;
+    private boolean automaticGearbox;
+
+    public Car(String colour, int doorsAmount, String brand, boolean automaticGearbox){
+        this.colour = colour;
+        this.doorsAmount = doorsAmount;
+        this.brand = brand;
+        this.automaticGearbox = automaticGearbox;
+    }
+
+    public void setAutomaticGearbox(boolean automaticGearbox) {
+        this.automaticGearbox = automaticGearbox;
+    }
+    public boolean getAutomaticGearbox(){
+        return automaticGearbox;
+    }
+
+
+}
